120fc51addd932f07d5323c019a0924429dd18d8
Steffen W <sateffen@gmail.com>
2017-04-25 22:08:02 +0200

    Added preparations for npm publish

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+/* global require, module*/
+const commonjs = require('rollup-plugin-commonjs');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const buble = require('rollup-plugin-buble');
+
+module.exports = {
+    entry: 'src/signalr.js',
+    format: 'umd',
+    dest: 'dist/signalr-extended.js',
+    moduleName: "SignalR",
+    plugins: [
+        nodeResolve(),
+        commonjs({
+            include: ['node_modules/**']
+        }),
+        buble()
+    ]
+};
